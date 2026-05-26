9c814a0a5afd5f6f78db8d3b694ecd570cc6e041
Rizal Anggoro <gnoogler4@gmail.com>
2026-05-26 17:17:23 +0700

    fix: reset default email and password values in LoginUiState

--- android/app/src/main/java/id/my/rizalanggoro/arta/feature/auth/presentation/login/LoginUiState.kt
+++ android/app/src/main/java/id/my/rizalanggoro/arta/feature/auth/presentation/login/LoginUiState.kt
@@ -1,8 +1,8 @@
 package id.my.rizalanggoro.arta.feature.auth.presentation.login
 
 data class LoginUiState(
-    val email: String = "[REDACTED]",
-    val password: String = "[REDACTED]",
+    val email: String = "",
+    val password: String = "",
     val emailError: String? = null,
     val passwordError: String? = null,
     val isLoading: Boolean = false,
