ab06045bee895e05846527e76506fdde1d94f6c2
Rizal Anggoro <gnoogler4@gmail.com>
2026-05-30 15:20:58 +0700

    chore: Update versionCode to 6 and versionName to 1.0.5 in build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "id.my.rizalanggoro.arta"
         minSdk = 29
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.0.4"
+        versionCode = 6
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
