a8f0961ea0f2b9e64e6bddcf31371c462b9af8ef
Rizal Anggoro <gnoogler4@gmail.com>
2026-05-31 21:32:59 +0700

    feat: Update versionCode and versionName to 7 and 1.0.6 respectively

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "id.my.rizalanggoro.arta"
         minSdk = 29
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.0.5"
+        versionCode = 7
+        versionName = "1.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
