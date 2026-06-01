e464823dae3b5c238c8fcd7ba55391794ffb5bed
Rizal Anggoro <gnoogler4@gmail.com>
2026-06-01 21:42:39 +0700

    chore: update versionCode to 9 and versionName to 1.0.8 in build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "id.my.rizalanggoro.arta"
         minSdk = 29
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.0.7"
+        versionCode = 9
+        versionName = "1.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
