54cadb6f8e972a3e0d84fe6241af8370d1d8cb99
Rizal Anggoro <gnoogler4@gmail.com>
2026-06-07 20:38:54 +0700

    feat: update version code and version name to 10 and 1.0.9 respectively

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "id.my.rizalanggoro.arta"
         minSdk = 29
         targetSdk = 36
-        versionCode = 9
-        versionName = "1.0.8"
+        versionCode = 10
+        versionName = "1.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
