44aaa3f46c2344c558c18e5edd4a76a4fe175e3a
Rizal Anggoro <gnoogler4@gmail.com>
2026-06-14 15:13:27 +0700

    feat: update versionCode and versionName to 12 and 1.0.12 respectively

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "id.my.rizalanggoro.arta"
         minSdk = 29
         targetSdk = 36
-        versionCode = 11
-        versionName = "1.0.10"
+        versionCode = 12
+        versionName = "1.0.12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
