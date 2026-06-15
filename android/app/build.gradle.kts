fbdca5cf8a185c5b2dbd427e90db0da4c66017f8
Rizal Anggoro <gnoogler4@gmail.com>
2026-06-15 11:46:04 +0700

    Bump Android app version to 1.0.13

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "id.my.rizalanggoro.arta"
         minSdk = 29
         targetSdk = 36
-        versionCode = 12
-        versionName = "1.0.12"
+        versionCode = 13
+        versionName = "1.0.13"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
