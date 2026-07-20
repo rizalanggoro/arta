6ff845cad470c9069a99c98d183c0c5c7b3430ae
Rizal Anggoro <gnoogler4@gmail.com>
2026-07-20 17:11:15 +0700

    fix: update Android app version to 1.0.19 and version code to 19

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "id.my.rizalanggoro.arta"
         minSdk = 29
         targetSdk = 36
-        versionCode = 18
-        versionName = "1.0.18"
+        versionCode = 19
+        versionName = "1.0.19"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
