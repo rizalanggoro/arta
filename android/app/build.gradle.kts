59b807e01207d1e4eb30cec5910171746f331d17
Rizal Anggoro <gnoogler4@gmail.com>
2026-07-20 08:25:56 +0700

    chore: bump version code to 16 and version name to 1.0.16

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "id.my.rizalanggoro.arta"
         minSdk = 29
         targetSdk = 36
-        versionCode = 15
-        versionName = "1.0.15"
+        versionCode = 16
+        versionName = "1.0.16"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
