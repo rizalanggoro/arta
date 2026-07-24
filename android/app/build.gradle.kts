9bb4f527655cccb27113cf0c55d68f95f509c509
Rizal Anggoro <gnoogler4@gmail.com>
2026-07-24 10:48:02 +0700

    fix: update Android app version to 1.1.0 and version code to 22

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "id.my.rizalanggoro.arta"
         minSdk = 29
         targetSdk = 36
-        versionCode = 21
-        versionName = "1.0.21"
+        versionCode = 22
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
